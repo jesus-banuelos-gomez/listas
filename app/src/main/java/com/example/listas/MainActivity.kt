f8d4a833feeb5bb614bc6d83ce8620bc48c2a2b6
jesus bañuelos <jesusbg1225dic@gmail.com>
2022-04-07 08:53:27 -0500

    aplicacion terminada

--- app/src/main/java/com/example/listas/MainActivity.kt
+++ app/src/main/java/com/example/listas/MainActivity.kt
@@ -16,6 +16,12 @@ class MainActivity : AppCompatActivity() {
 
         listaPersonaje.add(Personaje("Bender", "https://img2.freepng.es/20171220/ake/futurama-bender-png-5a3a03f952e1b6.81399659151375154533958088.jpg"))
         listaPersonaje.add(Personaje("Homero", "https://img2.freepng.es/20181204/vo/kisspng-higher-maths-clip-art-mathematics-yahoo-answers-5c064d3aa59ad1.3031538515439168586783.jpg"))
+        listaPersonaje.add(Personaje("Bender", "https://img2.freepng.es/20171220/ake/futurama-bender-png-5a3a03f952e1b6.81399659151375154533958088.jpg"))
+        listaPersonaje.add(Personaje("Homero", "https://img2.freepng.es/20181204/vo/kisspng-higher-maths-clip-art-mathematics-yahoo-answers-5c064d3aa59ad1.3031538515439168586783.jpg"))
+        listaPersonaje.add(Personaje("Bender", "https://img2.freepng.es/20171220/ake/futurama-bender-png-5a3a03f952e1b6.81399659151375154533958088.jpg"))
+        listaPersonaje.add(Personaje("Homero", "https://img2.freepng.es/20181204/vo/kisspng-higher-maths-clip-art-mathematics-yahoo-answers-5c064d3aa59ad1.3031538515439168586783.jpg"))
+        listaPersonaje.add(Personaje("Bender", "https://img2.freepng.es/20171220/ake/futurama-bender-png-5a3a03f952e1b6.81399659151375154533958088.jpg"))
+        listaPersonaje.add(Personaje("Homero", "https://img2.freepng.es/20181204/vo/kisspng-higher-maths-clip-art-mathematics-yahoo-answers-5c064d3aa59ad1.3031538515439168586783.jpg"))
 
 
         miRecycler =findViewById(R.id.RecyclerPersonajes)
